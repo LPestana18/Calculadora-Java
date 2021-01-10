f0c6a2c3174bae68a867298e2ff407fb63442a48
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-01-10 20:25:40 -0300

    Criando os Componentes
    Criando os Componentos Display Botão e Teclado

---
+++ src/br/com/lucaspestana/calc/visao/Botao.java
@@ -0,0 +1,7 @@
+package br.com.lucaspestana.calc.visao;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton {
+}
---
+++ src/br/com/lucaspestana/calc/visao/Teclado.java
@@ -0,0 +1,10 @@
+package br.com.lucaspestana.calc.visao;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel {
+
+    public Teclado() {
+    }
+}
