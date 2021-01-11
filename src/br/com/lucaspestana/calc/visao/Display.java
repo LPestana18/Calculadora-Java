d4cf0aa3554ddc7112f319b62c377fdc0daaccb2
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-01-10 23:18:26 -0300

    Logica Calculadora #02

--- src/br/com/lucaspestana/calc/visao/Display.java
+++ src/br/com/lucaspestana/calc/visao/Display.java
@@ -1,16 +1,19 @@
 package br.com.lucaspestana.calc.visao;
 
 import br.com.lucaspestana.calc.modelo.Memoria;
+import br.com.lucaspestana.calc.modelo.MemoriaObservador;
 
 import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("serial")
-public class Display extends JPanel {
+public class Display extends JPanel implements MemoriaObservador {
 
     private final JLabel label;
 
     public Display() {
+        Memoria.getInstancia().adicionarObservador(this);
+
         setBackground(new Color(46, 49, 50));
         label = new JLabel(Memoria.getInstancia().getTextoAtual());
         label.setForeground(Color.white);
@@ -19,5 +22,10 @@ public class Display extends JPanel {
         setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
 
         add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
     }
 }
