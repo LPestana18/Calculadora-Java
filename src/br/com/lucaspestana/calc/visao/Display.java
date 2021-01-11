14986bfdaf361dc8035fcedef953f1f9164fbb0e
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-01-10 21:59:01 -0300

    Componente Display

--- src/br/com/lucaspestana/calc/visao/Display.java
+++ src/br/com/lucaspestana/calc/visao/Display.java
@@ -1,10 +1,21 @@
 package br.com.lucaspestana.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+    private final JLabel label;
+
     public Display() {
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel("122334,44");
+        label.setForeground(Color.white);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+        add(label);
     }
 }
