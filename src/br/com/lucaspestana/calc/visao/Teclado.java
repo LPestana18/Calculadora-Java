48a3a22f5626d576a40f0dcde874f33d577412ca
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-01-10 22:39:24 -0300

    Componente Teclado #01

--- src/br/com/lucaspestana/calc/visao/Teclado.java
+++ src/br/com/lucaspestana/calc/visao/Teclado.java
@@ -12,33 +12,84 @@ public class Teclado extends JPanel {
 
     public Teclado() {
 
-        setLayout(new GridLayout(5, 4));
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
 
-        add(new Botao("AC", COR_CINZA_ESCURO));
-        add(new Botao("+/-", COR_CINZA_ESCURO));
-        add(new Botao("%", COR_CINZA_ESCURO));
-        add(new Botao("/", COR_LARANJA));
+        setLayout(layout);
 
-        add(new Botao("7", COR_CINZA_CLARO));
-        add(new Botao("8", COR_CINZA_CLARO));
-        add(new Botao("9", COR_CINZA_CLARO));
-        add(new Botao("*", COR_LARANJA));
+        // Linha 0
+        adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
+        adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
+        adicionarBotao("%", COR_LARANJA, c, 2, 0);
+        adicionarBotao("/", COR_LARANJA, c, 3, 0);
 
-        add(new Botao("4", COR_CINZA_CLARO));
-        add(new Botao("5", COR_CINZA_CLARO));
-        add(new Botao("6", COR_CINZA_CLARO));
-        add(new Botao("-", COR_LARANJA));
+        // Linha 1
+        adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+        adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+        adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
+        adicionarBotao("*", COR_LARANJA, c, 3, 1);
 
-        add(new Botao("1", COR_CINZA_CLARO));
-        add(new Botao("2", COR_CINZA_CLARO));
-        add(new Botao("3", COR_CINZA_CLARO));
-        add(new Botao("+", COR_LARANJA));
+        // Linha 2
+        adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+        adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+        adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
+        adicionarBotao("*", COR_LARANJA, c, 3, 1);
 
-        add(new Botao("0", COR_CINZA_CLARO));
-        add(new Botao(",", COR_CINZA_CLARO));
-        add(new Botao("*", COR_LARANJA));
-        add(new Botao("=", COR_LARANJA));
+        // Linha 3
+        adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
+        adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
+        adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
+        adicionarBotao("-", COR_LARANJA, c, 3, 2);
 
+        // Linha 4
+        adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
+        adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
+        adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
+        adicionarBotao("+", COR_LARANJA, c, 3, 3);
 
+        // Linha 5
+        adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
+        adicionarBotao(",", COR_CINZA_CLARO, c, 1, 4);
+        adicionarBotao("=", COR_LARANJA, c, 2, 4);
+
+        c.gridy = 0;
+        c.gridx = 0;
+        add(new Botao("AC", COR_CINZA_ESCURO), c);
+        c.gridx = 1;
+        add(new Botao("+/-", COR_CINZA_ESCURO), c);
+        c.gridx = 2;
+        add(new Botao("%", COR_CINZA_ESCURO), c);
+        c.gridx = 3;
+        add(new Botao("/", COR_LARANJA), c);
+
+        c.gridy = 1;
+        c.gridx = 0;
+        add(new Botao("7", COR_CINZA_CLARO), c);
+        add(new Botao("8", COR_CINZA_CLARO), c);
+        add(new Botao("9", COR_CINZA_CLARO), c);
+        add(new Botao("*", COR_LARANJA), c);
+
+        add(new Botao("4", COR_CINZA_CLARO), c);
+        add(new Botao("5", COR_CINZA_CLARO), c);
+        add(new Botao("6", COR_CINZA_CLARO), c);
+        add(new Botao("-", COR_LARANJA), c);
+
+        add(new Botao("1", COR_CINZA_CLARO), c);
+        add(new Botao("2", COR_CINZA_CLARO), c);
+        add(new Botao("3", COR_CINZA_CLARO), c);
+        add(new Botao("+", COR_LARANJA), c);
+
+        add(new Botao("0", COR_CINZA_CLARO), c);
+        add(new Botao("0", COR_CINZA_CLARO), c);
+        add(new Botao(",", COR_CINZA_CLARO), c);
+        add(new Botao("*", COR_LARANJA), c);
+        add(new Botao("=", COR_LARANJA), c);
+    }
+
+    private void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
+        c.gridx = x;
+        c.gridy = y;
+        Botao botao = new Botao(texto, cor);
+        add(botao, c);
     }
 }
