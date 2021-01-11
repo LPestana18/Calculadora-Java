10ee6272bfe3bbeb7b12bc341487ed822df2f706
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-01-10 23:33:16 -0300

    Lógica da Calculadora #03

--- src/br/com/lucaspestana/calc/modelo/Memoria.java
+++ src/br/com/lucaspestana/calc/modelo/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando {
+        ZERAR, NUMERO, DIVISAO, MULT, SUB, SOMA, IGUAL, VIRGULA;
+    };
+
     private static final Memoria instancia = new Memoria();
 
     private final List<MemoriaObservador> observadores = new ArrayList<>();
@@ -26,13 +30,47 @@ public class Memoria {
         return textoAtual.isEmpty() ? "0" : textoAtual;
     }
 
-    public void processarComando(String valor) {
+    public void processarComando(String texto) {
 
-        if ("AC".equals(valor)) {
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
+
+        if ("AC".equals(texto)) {
             textoAtual = "";
         }else {
-            textoAtual += valor;
+            textoAtual += texto;
         }
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+
+        if (textoAtual.isEmpty() && texto == "0") {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        }catch (NumberFormatException e) {
+            // Quando não for número...
+            if ("AC".equals(texto)) {
+                return TipoComando.ZERAR;
+            }else if("/".equals(texto)) {
+                return TipoComando.DIVISAO;
+            }else if("*".equals(texto)) {
+                return TipoComando.MULT;
+            }else if ("-".equals(texto)) {
+                return TipoComando.SUB;
+            }else if("=".equals(texto)) {
+                return TipoComando.IGUAL;
+            }else if(",".equals(texto)){
+                return TipoComando.VIRGULA;
+            }else if("+".equals(texto)) {
+                return TipoComando.SOMA;
+            }
+        }
+
+        return null;
     }
 }
